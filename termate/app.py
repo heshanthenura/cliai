9c57e6830325cec7c6f9f55fa93f28534b83afb5
Heshan Thenura <heshanthenura@protonmail.com>
2025-09-18 16:29:03 +0530

    fix: add linux only enforcement

--- termate/app.py
+++ termate/app.py
@@ -1,4 +1,10 @@
+import platform
+import sys
 from .utils.userCLI import start_cli
+
+if platform.system() != "Linux":
+    print("⚠️ TerMate only works on Linux!")
+    sys.exit(1)
 
 if __name__ == "__main__":
     start_cli()
